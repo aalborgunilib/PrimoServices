d823c8f5ff301ca9991d4c651329bea5472366f8
Kasper Løvschall <kl@aub.aau.dk>
2015-03-11 14:05:38 +0100

    $data was used as a reference - changed to $data_ref

--- lib/PrimoServices.pm
+++ lib/PrimoServices.pm
@@ -17,37 +17,37 @@ use version; our $VERSION = version->declare("v2.2.0");
 
 get qr{.*} => sub {
 	
-	my $data;
+	my $data_ref;
 
 	# Parse and reply to the request
-	#$data = PrimoServices::Dispatcher::handle_request();
+	#$data_ref = PrimoServices::Dispatcher::handle_request();
 	
 	try {
 		my $execution_time = timeit( 1, sub{
-			$data = PrimoServices::Dispatcher::handle_request();
+			$data_ref = PrimoServices::Dispatcher::handle_request();
 		});
 
 		# Calculate the total execution time of the call
-		$data->{appExecutionTime} = sprintf "%.3fs", $execution_time->real;
+		$data_ref->{appExecutionTime} = sprintf "%.3fs", $execution_time->real;
 	}
 	catch {
 		status 500;
-		$data->{error} = 'PrimoServices';
-		$data->{exception} = $_;
+		$data_ref->{error} = 'PrimoServices';
+		$data_ref->{exception} = $_;
 	};
 
 	# Insert the request into the response (in development)
-	$data->{request} = params if config->{environment} eq 'development';
+	$data_ref->{request} = params if config->{environment} eq 'development';
 
 	# Delete request_lookup_table (outside development)
-	delete $data->{request_lookup_table} if config->{environment} ne 'development';
+	delete $data_ref->{request_lookup_table} if config->{environment} ne 'development';
 
 	# Count requested and returned number of IDs
-	$data->{totalItems}	= param_array 'id';
-	$data->{totalItemsReturned} = @{$data->{items}};
+	$data_ref->{totalItems}	= param_array 'id';
+	$data_ref->{totalItemsReturned} = @{$data_ref->{items}};
 
 	# Name and version
-	$data->{appName} = config->{appname} . ' ' . $VERSION->normal();
+	$data_ref->{appName} = config->{appname} . ' ' . $VERSION->normal();
 
 	# Set the serializer to either JSONP or JSON
 	if ( params->{callback} ) {
@@ -58,7 +58,7 @@ get qr{.*} => sub {
 	}
 
 	# Serialize...
-	return $data;
+	return $data_ref;
 };
 
 true;
